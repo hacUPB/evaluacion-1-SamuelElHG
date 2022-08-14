8097fa76a3c11b814d0a4dd3daf74f5b2759ecf9
SamuelElHG <samuel.hernandezg@upb.edu.co>
2022-08-14 17:54:11 -0500

    ya no sé que hago

--- dirTest/project/wcat.c
+++ dirTest/project/wcat.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char *argv[]){
-      FILE *fin = fopen(argv[1],"r");
+
+    FILE *fin = fopen(argv[1],"r");
 
     if(fin == NULL){
         perror("fopen-fin fails: ");
